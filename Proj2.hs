75fd43e6a862214169e217d6a04f05e8e0b2e6b9
jonjau <jonjau3Q@gmail.com>
2020-10-06 20:53:17 +1100

    added spec and template

---
+++ Proj2.hs
@@ -0,0 +1,7 @@
+--Implement your solution here
+--SEE THE PROJECT CODING GUIDELINES ON THE LMS FOR DETAILS OF
+--THE CRITERIA THAT WILL BE EMPLOYED IN ASSESSING YOUR CODE.
+--Please DELETE THIS WHOLE COMMENT, and write your own.
+
+module Proj2 (Pitch, toPitch, feedback,
+              GameState, initialGuess, nextGuess) where
